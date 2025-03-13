44c906ae48c4d654f5e806502612c84a7a7281fa
dondonherrera <dondonherrera02@gmail.com>
2025-03-13 23:41:49 +0800

    Initial file system changes

--- js/ReadMe.txt
+++
@@ -1,9 +0,0 @@
-Checklist:
-
-Make sure these are included in the HTML to avoid issues:
-
-Bootstrap
-CSS
-JavaScript (JS)
-Alertify
-jQuery
--- js/owner.js
+++
@@ -1,6 +0,0 @@
-/**
-* @name: Co-Space Web App - Owner JS
-* @Course Code: SODV1201
-* @class: Software Development Diploma program.
-* @author: Dondon Herrera
-*/
